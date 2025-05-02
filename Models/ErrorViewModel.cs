1c917e0781f1bd10b2c1d18df789ab5c91cd4819
Nupur <nupur.yaadav@gmail.com>
2025-05-02 10:53:53 -0700

    .NET project setup.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AutodeskWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

854f678da0bedf6a28c0cb30e2611365399a5ba8
Nupur <nupur.yaadav@gmail.com>
2025-05-07 13:37:37 -0700

    Move project under a root folder.

---
+++ AutodeskWebApp/Controllers/HomeController.cs
@@ -0,0 +1,314 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AutodeskWebApp.Models;
+using AutodeskWebApp.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutodeskWebApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly DriverData dbcontext;
+    public List<Race> race;
+
+    private Dictionary<Guid, string> driverHeadhsotGuids = new Dictionary<Guid, string>();
+    private HttpClient client = new HttpClient();
+
+    public HomeController(DriverData context)
+    {
+        this.dbcontext = context;
+    }
+    
+    [HttpGet]
+    public IActionResult AddDriver()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddDriver(Driver driver)
+    {
+        try
+        {
+            var driverObj = new Driver
+            {
+                Id = Guid.NewGuid(), // Generate a new GUID for the driver ID
+                Name = driver.Name,
+                Email = driver.Email,
+                Phone = driver.Phone,
+                Team = driver.Team,
+                HomeCountry = driver.HomeCountry,
+                IsRacingThisYear = driver.IsRacingThisYear,
+                ImageUrl = driver.ImageUrl,
+                DriverNumber = driver.DriverNumber
+            };
+
+            await dbcontext.Drivers.AddAsync(driverObj);
+            await dbcontext.SaveChangesAsync(); // Save changes to the database
+            
+            // Redirect to the driver list page after editing
+            return RedirectToAction("DriverList", "Home");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DriverList(int? driverNumber, Guid? driverId) // Find driver by driver number OR driver ID
+    {
+
+        List<Driver> drivers = new List<Driver>();
+
+        try
+        {
+            // Initialize the dictionary with driver headshot URLs
+            if (driverHeadhsotGuids.Count == 0)
+            {
+                foreach (var driver in await dbcontext.Drivers.ToListAsync())
+                {
+                    driverHeadhsotGuids[driver.Id] = driver.ImageUrl;
+                }
+            }
+
+            // Get driver by driver number
+            if (driverNumber != null)
+            {
+                drivers = await dbcontext.Drivers.Where(driver => driver.DriverNumber == driverNumber).ToListAsync();
+                // If driver number is not found, return all drivers
+                if (driverNumber == 0) 
+                {
+                    drivers = await dbcontext.Drivers.ToListAsync();
+                }
+            }
+            // Get driver by driver ID
+            else if (driverId != null)
+            {
+                drivers = await dbcontext.Drivers.Where(driver => driver.Id == driverId).ToListAsync();
+                // If driver number is not found, return all drivers
+                if (driverId == Guid.Empty) 
+                {
+                    drivers = await dbcontext.Drivers.ToListAsync();
+                }
+            }
+            else
+            {
+                // Get all drivers
+                drivers = await dbcontext.Drivers.ToListAsync();
+            }
+            
+            // Sort by driver number
+            drivers = drivers.OrderBy(driver => driver.DriverNumber).ToList();
+            return View(drivers);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> TeamList(int teamRank)
+    {
+        // Initialize the dictionary with driver headshot URLs
+        if (driverHeadhsotGuids.Count == 0)
+        {
+            foreach (var driver in await dbcontext.Drivers.ToListAsync())
+            {
+                driverHeadhsotGuids[driver.Id] = driver.ImageUrl;
+            }
+        }
+
+        
+        List<TeamView> teamViews = [];
+        List<Team> teams = [];
+        teams = teamRank == 0 ? await dbcontext.Teams.ToListAsync() : await dbcontext.Teams.Where(team => team.Rank == teamRank).ToListAsync();
+
+        // Initialize TeamView objects
+        foreach (var team in teams)
+        {
+            var teamView = new TeamView
+            {
+                    Id = team.Id,
+                    Name = team.Name,
+                    Driver1 = driverHeadhsotGuids.TryGetValue(team.Driver1, out string? value) ? value : "Unknown",
+                    Driver1Number = team.Driver1,
+                    Driver2 = driverHeadhsotGuids.TryGetValue(team.Driver2, out string? value1) ? value1 : "Unknown",
+                    Driver2Number = team.Driver2,
+                    TeamChief = team.TeamChief,
+                    CarImageUrl = team.CarImageUrl,
+                    Rank = team.Rank,
+                    PolePositions = team.PolePositions
+            };
+            Console.WriteLine($"Team: {team.CarImageUrl}");
+            teamViews.Add(teamView);
+        }
+
+        // Sort by team ranking
+        teamViews = teamViews.OrderBy(team => team.Rank).ToList();
+
+        return View(teamViews);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> EditDriver(Guid id)
+    {
+        var driver = await dbcontext.Drivers.FindAsync(id);
+        Debug.Assert(driver != null, "Driver not found in the database.");
+        if (driver == null)
+        {
+            return NotFound();
+        }
+        return View(driver);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditDriver(Driver driver)
+    {
+        var existingDriver = await dbcontext.Drivers.FindAsync(driver.Id);
+        if (existingDriver == null)
+        {
+            return NotFound();
+        }
+        // Update the existing driver properties with the new values
+        existingDriver.Name = driver.Name;
+        existingDriver.Email = driver.Email;
+        existingDriver.Phone = driver.Phone;
+        existingDriver.Team = driver.Team;
+        existingDriver.HomeCountry = driver.HomeCountry;
+        existingDriver.IsRacingThisYear = driver.IsRacingThisYear;
+        existingDriver.ImageUrl = driver.ImageUrl;
+        existingDriver.DriverNumber = driver.DriverNumber;
+
+        // Save the changes to the database
+        await dbcontext.SaveChangesAsync();
+
+        // Redirect to the driver list page after editing
+        return RedirectToAction("DriverList", "Home");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteDriver(Guid id)
+    {
+        var driver = await dbcontext.Drivers.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        if (driver == null)
+        {
+            return NotFound();
+        }
+
+        dbcontext.Drivers.Remove(driver);
+        await dbcontext.SaveChangesAsync();
+
+        return RedirectToAction("DriverList", "Home");
+    }
+   
+   [HttpGet]
+   public async Task<IActionResult> RaceList(DateOnly? startDate, DateOnly? endDate)
+   {   
+        try
+        {
+            // Validate date
+            if (startDate == null) startDate = DateOnly.MinValue;
+            if (endDate == null) endDate = DateOnly.FromDateTime(DateTime.Now);
+
+            if (startDate > endDate)
+            {
+                Console.WriteLine("Start date must be before end date.");
+                return View("Error", new ErrorViewModel { ErrorMessage = "Start date must be before end date." });
+            }
+            
+            // Setup URL
+            var raceListUrl = $"https://api.openf1.org/v1/sessions?date_start%3E%3D{startDate}&date_end%3C%3D{endDate}";
+            
+            HttpResponseMessage response = await client.GetAsync(raceListUrl);
+        
+            // Validate response
+            if (response.IsSuccessStatusCode)
+            {
+                race = await response.Content.ReadAsAsync<List<Race>>();
+            }
+            else{
+                Console.WriteLine($"Error: {response.StatusCode}");
+                return View("Error", new ErrorViewModel { ErrorMessage = $"Error: {response.StatusCode}" });
+            }
+            
+            return View(race);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+   }
+
+   public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> InitDatabase()
+    {   
+            try
+            {
+
+                // Setup URL
+                var raceListUrl = "https://api.openf1.org/v1/drivers";
+                
+                HttpResponseMessage response = await client.GetAsync(raceListUrl);
+            
+                // Validate response
+                if (response.IsSuccessStatusCode)
+                {
+                    var drivers = await response.Content.ReadAsAsync<List<JsonDriverData>>();
+                    foreach (var driver in drivers)
+                    {
+                        if (driver.headshot_url != null)
+                        {
+                            var driverObj = new Driver
+                            {
+                                Id = Guid.NewGuid(), // Generate a new GUID for the driver ID
+                                Name = driver.full_name,
+                                Email = driver.name_acronym + "@" + driver.team_name + ".com",
+                                Phone = driver.session_key.ToString(),
+                                Team = driver.team_name,
+                                HomeCountry = driver.country_code,
+                                IsRacingThisYear = true,
+                                ImageUrl = driver.headshot_url,
+                                DriverNumber = driver.driver_number
+                            };
+
+                            await dbcontext.Drivers.AddAsync(driverObj);
+                            await dbcontext.SaveChangesAsync(); // Save changes to the database}
+                        }
+                        
+                    }
+                        // Redirect to the driver list page after editing
+                        return RedirectToAction("DriverList", "Home");
+                }
+                else{
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                    return View("Error", new ErrorViewModel { ErrorMessage = $"Error: {response.StatusCode}" });
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+    }
+}
